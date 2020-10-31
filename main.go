3a0c6f2e39262730773da3b37450da44c80de3cc
AlfieriChou <alfierichou@gmail.com>
2020-10-31 17:59:31 +0800

    feat: graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
 	"github.com/AlfieriChou/go-galen/routes"
 )
 
 func main() {
-	routes.Run()
+	router := routes.CreateRouter()
+	server := &http.Server{
+		Addr:    ":4000",
+		Handler: router,
+	}
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		log.Println("receive interrupt signal")
+		if err := server.Close(); err != nil {
+			log.Fatal("Server Close:", err)
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
+	}
+
+	log.Println("Server exiting")
 }
--- routes/main.go
+++ routes/main.go
@@ -8,11 +8,11 @@ var (
 	router = gin.Default()
 )
 
-// Run 导出路由方法
-func Run() {
+// CreateRouter 创建路由方法
+func CreateRouter() *gin.Engine {
 	v1 := router.Group("/v1")
 	addPingRoutes(v1)
 	v2 := router.Group("/v2")
 	addPingRoutes(v2)
-	router.Run(":3000")
+	return router
 }
