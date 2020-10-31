9634d697b8a9ee69c14730b429372a2afb04813d
AlfieriChou <alfierichou@gmail.com>
2020-10-31 11:21:27 +0800

    chore: update

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Data struct {
+  message string
+}
+
 func main() {
-	r := gin.Default()
+  var data Data
+  r := gin.Default()
+  data.message = "pong"
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			code:    200,
-			message: "ok",
-			data: {
-				message: "pong",
-			},
+      "code": 200,
+      "message": "ok",
+      "data": data,
 		})
 	})
 	r.Run(":3000")
